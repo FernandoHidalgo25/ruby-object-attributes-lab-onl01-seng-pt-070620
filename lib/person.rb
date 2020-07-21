aeeac9ea5b9669cc08b7c46ece9975b359afe000
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-21 05:50:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+ 
+  def name=(person_name)
+    @name = person_name
+  end
+ 
+  def name
+    @person_name
+  end
+  
+    def name=(person_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
